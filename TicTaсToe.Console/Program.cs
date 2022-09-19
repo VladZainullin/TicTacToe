dd438879fff6f0a9df10294d8dc08742573205b6
Vladislav Zainullin <mittoface02@mail.ru>
2022-09-19 20:54:39 +0300

    #main Изменяет комментарии

--- TicTaсToe.Console/Program.cs
+++ TicTaсToe.Console/Program.cs
@@ -18,20 +18,20 @@ var game = new Game(players, 3);
 
 while (game.Status != GameStatus.Stop)
 {
-    Console.WriteLine($"Move {game.CurrentPlayer.Name}. Please, enter the coordinate");
+    Console.WriteLine($"Move \"{game.CurrentPlayer.Name}\". Please, enter the coordinate");
 
     Console.Write("X: ");
-    var x = int.Parse(Console.ReadLine() ?? throw new ArgumentException("Ошибка ввода X"));
+    var x = int.Parse(Console.ReadLine() ?? throw new ArgumentException("Incorrect coordinate X"));
 
     Console.Write("Y: ");
-    var y = int.Parse(Console.ReadLine() ?? throw new ArgumentException("Ошибка ввода Y"));
+    var y = int.Parse(Console.ReadLine() ?? throw new ArgumentException("Incorrect coordinate Y"));
     
     Console.Write("Z: ");
-    var z = int.Parse(Console.ReadLine() ?? throw new ArgumentException("Ошибка ввода Y"));
+    var z = int.Parse(Console.ReadLine() ?? throw new ArgumentException("Incorrect coordinate Z"));
 
     var point = new Point(x, y, z);
-    
+
     game.MakeMove(point);
 }
 
-Console.WriteLine($"{game.CurrentPlayer.Name} win!");
+Console.Write($"Player \"{game.CurrentPlayer.Name}\" win!");
